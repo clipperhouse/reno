6002e40f2fb945b54dd01db5448499d0efc2a3d7
Matt Sherman <mwsherman@gmail.com>
2014-09-22 23:26:19 -0400

    something like a proof of concept

--- js/reno.js
+++ js/reno.js
@@ -1,18 +1,57 @@
-var typeChar = function ($el) {
-	var text = $el.data('text');
-	var pos = $el.data('pos');
-	$el.text($el.text() + text.charAt(pos));
-	if (pos < text.length) {
-		pos++;
-		$el.data('pos', pos);
-		setTimeout(function() {
-			typeChar($el);
-		}, 20);		
-	}
-};
+var Presentation = function(src) {
+    var blocks = [];
+    var blockComment = /\s*\/\/\s*block\s*/;
 
-var type = function  (text, $el) {
-	$el.data('text', text);
-	$el.data('pos', 0);
-	typeChar($el);
-};
+    // parse incoming src
+    (function() {
+        var srcLines = src.split('\n');
+        var blockLines = [];
+
+        for (var i = 0; i < srcLines.length; i++) {
+            var line = srcLines[i];
+            if (line.match(blockComment)) {
+                if (blockLines.length) {
+                    blocks.push(blockLines.join('\n'));
+                }
+                blockLines = [];
+            } else {
+                blockLines.push(line);
+            }
+        }
+
+        // last one
+        if (blockLines.length) {
+            blocks.push(blockLines.join('\n'));
+        }
+    })();
+
+    // stateful bits for below
+	var blockIndex = 0;
+	var pos = 0;
+	var $out;
+
+	var typeBlocks = function() {
+		if (blockIndex > blocks.length - 1) return;
+
+        $out.text($out.text() + blocks[blockIndex].charAt(pos));
+
+        if (pos < blocks[blockIndex].length) {
+            pos++;
+            setTimeout(function() {
+                typeBlocks();
+            }, 20);
+
+            return;
+        }
+
+        pos = 0;
+        blockIndex++;
+        typeBlocks();
+    };
+
+    this.typeInto = function($output) {
+    	$output.text('');
+    	$out = $output;
+      	typeBlocks();
+    }
+}
